0ba24ade6afd1103357f49af50aacb50f4e2a1a0
Rezende <rezende_netto@hotmail.com>
2021-07-15 17:01:07 -0300

    Solve exercise 32 - async await

--- exercicio-32/app.js
+++ exercicio-32/app.js
@@ -20,3 +20,182 @@
       listados na documentação: https://developers.giphy.com/docs/api/endpoint#search
   - Ignore os avisos no console. Para limpá-lo, pressione "ctrl + L".
 */
+
+// ############################
+// ########## CÓDIGO ##########
+// ############################
+
+// const form = document.querySelector('form');
+// const GIFSContainer = document.querySelector('div');
+
+// form.addEventListener('submit', async event => {
+//     event.preventDefault();
+
+//     const inputValue = event.target.search.value;
+//     const APIKey = 'ikdAKMVfS5f4O9UoM5PeGXJrFtXFDzgS';
+//     const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=1&q=${inputValue}`;
+
+//     try {
+//         const response = await fetch(url);
+
+//         if (!response.ok) {
+//             throw new Error('Não foi possível obter os dados da API');
+//         }
+
+//         const GIFData = await response.json();
+//         const downsizedGIFUrl = GIFData.data[0].images.downsized.url;
+//         const img = document.createElement('img');
+
+//         img.setAttribute('src', downsizedGIFUrl);
+//         img.setAttribute('alt', GIFData.data[0].title);
+
+//         GIFSContainer.insertAdjacentElement('afterbegin', img);
+//         console.log(img);
+
+//         event.target.reset();
+//     } catch (error) {
+//         alert(`Erro: ${error.message}`);
+//     }
+// });
+
+// #######################################
+// ########## CÓDIGO REFATORADO ##########
+// #######################################
+
+// const form = document.querySelector('form');
+// const GIFSContainer = document.querySelector('div');
+
+// // ***indicado separar por uma linha, já que acima são const DOM e abaixo const normal
+
+// // consts movidas ora fora da função
+// const APIKey = 'ikdAKMVfS5f4O9UoM5PeGXJrFtXFDzgS';
+
+// // ***indicado separar por uma linha, já que acima const e abaixo função
+
+// //prettier-ignore
+// // transformar a const url em função pra poder passar o inputValue que agora vai se chamar de GIFName
+// // const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=1&q=${inputValue}`;
+// const getGIPHYApiUrl = (GIFName) =>
+//   `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=1&q=${GIFName}`;
+
+// const generateGIFImg = (downsizedGIFUrl, GIFData) => {
+//     const img = document.createElement('img');
+
+//     img.setAttribute('src', downsizedGIFUrl);
+//     img.setAttribute('alt', GIFData.data[0].title);
+
+//     return img; // fazer retornar img pra poder usar fora da função
+// };
+
+// // o try catch vai ficar numa função isolada, porque os erros que preciso pega são os erros apenas da requisição
+// const fetchGIF = async inputValue => {
+//     try {
+//         const GIPHYApiUrl = getGIPHYApiUrl(inputValue);
+//         const response = await fetch(GIPHYApiUrl);
+
+//         if (!response.ok) {
+//             throw new Error('Não foi possível obter os dados da API'); // se cair aqui, nenhuma linha abaixo será executada, throw é semelhante ao return, mas não retorna nenhum valor
+//         }
+//         return response.json(); // colocar pra retornar
+//     } catch (error) {
+//         alert(`Erro: ${error.message}`); // retorna undefined, e undefined vai ser atribuído a const GIFData na invocação da fetchGIF(), pra evitar isso, precisamos utilizar um condicional if
+//     }
+// };
+
+// const insertGIFIntoDOM = async inputValue => {
+//     // isolar o código que está fazendo o request, try e catch, em uma função de responsabilidade única
+//     // try {
+//     //     const GIPHYApiUrl = getGIPHYApiUrl(inputValue);
+//     //     const response = await fetch(GIPHYApiUrl);
+
+//     //     if (!response.ok) {
+//     //         throw new Error('Não foi possível obter os dados da API');
+//     //     }
+
+//     const GIFData = await fetchGIF(inputValue); // fazer receber a invocação da fetchGIF
+
+//     if (GIFData) {
+//         const downsizedGIFUrl = GIFData.data[0].images.downsized.url; // quando cai no catch, vai dar problema aqui, pois GIFData armazena undefined e não é possível ler a propriedade data de undefined
+
+//         const img = generateGIFImg(downsizedGIFUrl, GIFData); // const img vai receber o retorno da função generateGIFImg
+
+//         GIFSContainer.insertAdjacentElement('afterbegin', img);
+//         console.log(img);
+
+//         // event.target.reset();
+//         // não vai mais funcionar com o event, substituir pela const form
+//         form.reset();
+//         // } catch (error) {
+//         //     alert(`Erro: ${error.message}`);
+//         // }
+//     }
+// };
+
+// // não precisa mais ser uma função async
+// form.addEventListener('submit', async event => {
+//     event.preventDefault();
+
+//     const inputValue = event.target.search.value;
+
+//     // const GIPHYApiUrl = getGIPHYApiUrl(inputValue); // mover pra dentro da função insertGIFIntoDOM
+//
+//     insertGIFIntoDOM(inputValue);
+// });
+
+// #########################################################
+// ########## CÓDIGO REFATORADO E SEM COMENTÁRIOS ##########
+// #########################################################
+
+const form = document.querySelector('form');
+const GIFSContainer = document.querySelector('div');
+
+const APIKey = 'ikdAKMVfS5f4O9UoM5PeGXJrFtXFDzgS';
+
+//prettier-ignore
+const getGIPHYApiUrl = GIFName => 
+  `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=1&q=${GIFName}`;
+
+const generateGIFUrl = (downsizedGIFUrl, GIFData) => {
+    const img = document.createElement('img');
+
+    img.setAttribute('src', downsizedGIFUrl);
+    img.setAttribute('alt', GIFData.data[0].title);
+
+    return img;
+};
+
+const fetchGIF = async inputValue => {
+    try {
+        const GIPHYApiUrl = getGIPHYApiUrl(inputValue);
+        const response = await fetch(GIPHYApiUrl);
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados da API');
+        }
+
+        return response.json();
+    } catch (error) {
+        alert(`Erro: ${error.message}`);
+    }
+};
+
+const insertGIFIntoDOM = async inputValue => {
+    const GIFData = await fetchGIF(inputValue);
+
+    if (GIFData) {
+        const downsizedGIFUrl = GIFData.data[0].images.downsized.url;
+        const img = generateGIFUrl(downsizedGIFUrl, GIFData);
+
+        GIFSContainer.insertAdjacentElement('afterbegin', img);
+
+        form.reset();
+    }
+};
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const inputValue = event.target.search.value;
+
+    insertGIFIntoDOM(inputValue);
+});
